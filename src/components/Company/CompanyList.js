9a0fa21fa7dac24a54bf9d17273b68ed31014ae1
luizfernando-o <luiz.2019230186@unicap.br>
2023-05-04 16:33:09 -0300

    reconfiguring API, enabling CORS

--- src/components/Company/CompanyList.js
+++ src/components/Company/CompanyList.js
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as CompanyServer from "./CompanyServer";
 const CompanyList = () => {
-  // const initialState = [
-  //   {
-  //     id: 1,
-  //     name: "Facebook",
-  //     foundation: 2004,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Google",
-  //     foundation: 2002,
-  //   },
-  // ];
-  //const [companies, setCompanies] = useState(initialState);
+ 
   const [companies, setCompanies] = useState([]);
   const listCompanies = async () => {
     try {
