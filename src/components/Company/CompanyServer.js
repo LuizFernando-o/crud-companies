a0e4bbe9ebff003ad92c5ced7777cbe4786ba309
luizfernando-o <luiz.2019230186@unicap.br>
2023-05-15 20:34:32 -0300

    new API_URL

--- src/components/Company/CompanyServer.js
+++ src/components/Company/CompanyServer.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api/companies/";
+const API_URL = "http://157.245.113.222:8000/api/companies/";
 
 export const listCompanies = async () => {
     return await fetch(API_URL);
